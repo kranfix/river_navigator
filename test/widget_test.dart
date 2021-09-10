477c24cf07c3e7e19cf49f9c5fedb40e74b959c7
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-10 18:35:23 -0500

    test: Page1 navigates to FlowPage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:river_navigator/main.dart';
+import 'package:river_navigator/src/flow_page.dart';
+import 'package:river_navigator/src/page1.dart';
 
 extension AppTester on WidgetTester {
   Future<void> pumpApp() {
@@ -11,28 +13,26 @@ extension AppTester on WidgetTester {
 }
 
 void main() {
-  testWidgets('Page1 counter increments', (WidgetTester tester) async {
-    await tester.pumpApp();
-
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-    expect(find.text('2'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('1'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
-    expect(find.text('3'), findsNothing);
-  });
-
   group('From Page1 to Page2', () {
+    testWidgets('Page1 to FlowPage', (WidgetTester tester) async {
+      await tester.pumpApp();
+
+      final page1 = find.byType(Page1);
+      expect(find.byType(Page1), findsOneWidget);
+
+      final goToFlowButton = find.descendant(
+        of: page1,
+        matching: find.byType(ElevatedButton),
+      );
+      expect(goToFlowButton, findsOneWidget);
+
+      await tester.tap(goToFlowButton);
+      await tester.pumpAndSettle(const Duration(milliseconds: 400));
+
+      final flowPage = find.byType(FlowPage);
+      expect(flowPage, findsOneWidget);
+    });
+
     testWidgets('Go to Page3, pop and pop must go to Page1',
         (WidgetTester tester) async {
       await tester.pumpApp();
@@ -40,11 +40,11 @@ void main() {
       final goToFlowButton = find.byType(ElevatedButton);
 
       await tester.tap(goToFlowButton);
-    });
+    }, skip: true);
 
     testWidgets('Replace to Page3 and pop must go to Page1',
         (WidgetTester tester) async {
       tester.pumpApp();
-    });
+    }, skip: true);
   });
 }
