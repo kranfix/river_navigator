b697b7fea9cfa91381589e4488233ba53ccaad3c
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-10 20:12:44 -0500

    more pre-tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:river_navigator/main.dart';
 import 'package:river_navigator/src/flow_page.dart';
 import 'package:river_navigator/src/page1.dart';
+import 'src/widgets/counter_scaffold_test.dart';
 
 extension AppTester on WidgetTester {
   Future<void> pumpApp() {
     return pumpWidget(const ProviderScope(child: RiverNavigatorApp()));
+  }
+
+  Future<Finder> fromPage1ToFlowPage({int counter = 0}) async {
+    await pumpApp();
+    final page1 = find.byType(Page1);
+    final goToFlowButton = find.descendant(
+      of: page1,
+      matching: find.byType(ElevatedButton),
+    );
+
+    if (counter > 0) {
+      incrementCounter(page1, times: counter);
+    }
+
+    await tap(goToFlowButton);
+    await pumpAndSettle(const Duration(milliseconds: 400));
+    final flowPage = find.byType(FlowPage);
+    return flowPage;
   }
 }
 
@@ -35,11 +54,9 @@ void main() {
 
     testWidgets('Go to Page3, pop and pop must go to Page1',
         (WidgetTester tester) async {
-      await tester.pumpApp();
+      final flowPage = await tester.fromPage1ToFlowPage(counter: 5);
 
-      final goToFlowButton = find.byType(ElevatedButton);
-
-      await tester.tap(goToFlowButton);
+      //await tester.tap(goToFlowButton);
     }, skip: true);
 
     testWidgets('Replace to Page3 and pop must go to Page1',
