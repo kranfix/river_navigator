c8b142e05bfd29ab86d10ec9d941a78bdaeab876
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-12 09:38:31 -0500

    WidgetTester.tapBackButton()

---
+++ test/helpers/helpers.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+extension DecendantFinder on Finder {
+  Finder descendantByType(Type type) {
+    return find.descendant(
+      of: this,
+      matching: find.byType(type),
+    );
+  }
+}
+
+extension AppTester on WidgetTester {
+  Future<void> tapBackButton(Finder page) async {
+    final popButton = page.descendantByType(BackButton);
+    expect(popButton, findsOneWidget);
+    await tap(popButton);
+    await pumpAndSettle(const Duration(milliseconds: 400));
+  }
+}
