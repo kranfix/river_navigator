32ab5cf211b87e252a4394d7a454a7beb4348666
Frank Moreno <frankmoreno1993@gmail.com>
2021-09-12 15:49:25 -0500

    litter refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'src/app/app.dart';
+import 'package:river_navigator/src/app/app.dart';
 
 void main() {
   runApp(const ProviderScope(child: RiverNavigatorApp()));
