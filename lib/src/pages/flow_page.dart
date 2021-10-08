536012646c0f629d039a8978100e9a52a5b5b389
Frank Moreno <frankmoreno1993@gmail.com>
2021-10-08 10:16:54 -0500

    CounterFlow is a composition of FlowProviderScope and FlowScope

--- lib/src/pages/flow_page.dart
+++ lib/src/pages/flow_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:river_navigator/src/flow_controller/flow_controller.dart';
 import 'package:river_navigator/src/pages/counter_flow/flow_provider_scope.dart';
 import 'package:river_navigator/src/pages/page2.dart';
 
-class CounterFlow extends StatefulWidget {
+class CounterFlow extends StatelessWidget {
   const CounterFlow({Key? key}) : super(key: key);
-
-  @override
-  _CounterFlowState createState() => _CounterFlowState();
 
   static Future<void> navigate(BuildContext context) {
     return Navigator.of(context).push(
@@ -17,52 +15,16 @@ class CounterFlow extends StatefulWidget {
   }
 
   static FlowController of(BuildContext context) {
-    return context.findAncestorStateOfType<_CounterFlowState>()!;
-  }
-}
-
-mixin FlowController {
-  void pop();
-  void push(Page<dynamic> page);
-  void replaceWith(Page<dynamic> page);
-}
-
-class _CounterFlowState extends State<CounterFlow> with FlowController {
-  final pages = <Page<dynamic>>[
-    const MaterialPage(child: Page2()),
-  ];
-
-  @override
-  void pop() {
-    if (pages.length == 1) {
-      Navigator.of(context).pop();
-    } else {
-      setState(() => pages.removeLast());
-    }
-  }
-
-  @override
-  void push(Page<dynamic> page) {
-    setState(() => pages.add(page));
-  }
-
-  @override
-  void replaceWith(Page<dynamic> page) {
-    setState(() => pages.last = page);
+    return FlowScope.of(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return FlowProviderScope(
-      child: Navigator(
-        pages: [
-          const MaterialPage(child: Offstage()),
-          ...pages,
+      child: FlowScope(
+        initialStack: [
+          const MaterialPage(child: Page2()),
         ],
-        onPopPage: (route, result) {
-          pop();
-          return route.didPop(result);
-        },
       ),
     );
   }
