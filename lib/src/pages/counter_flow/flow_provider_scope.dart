08857030d0d14eb6a8c6d8308791f47f633af665
Frank Moreno <frankmoreno1993@gmail.com>
2021-11-25 10:25:15 -0500

    FlowProviderScope is StatelessWidget

--- lib/src/pages/counter_flow/flow_provider_scope.dart
+++ lib/src/pages/counter_flow/flow_provider_scope.dart
@@ -2,31 +2,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:river_navigator/src/widgets/widgets.dart';
 
-class FlowProviderScope extends ConsumerStatefulWidget {
+class FlowProviderScope extends StatelessWidget {
   const FlowProviderScope({Key? key, required this.child}) : super(key: key);
 
   final Widget child;
 
   @override
-  _FlowProviderScopeState createState() => _FlowProviderScopeState();
-}
-
-class _FlowProviderScopeState extends ConsumerState<FlowProviderScope> {
-  final counterValue = StateController(0);
-
-  @override
-  void dispose() {
-    super.dispose();
-    counterValue.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return ProviderScope(
-      overrides: [
-        counterProvider.overrideWithValue(counterValue),
-      ],
-      child: widget.child,
+      overrides: [counterProvider],
+      child: child,
     );
   }
 }
